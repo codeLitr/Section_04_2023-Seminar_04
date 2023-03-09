844f5dae8d4b2fa1714e59f9a4a421a3af4e72fd
SMV <kraft088@gmail.com>
2023-03-09 22:16:18 +1100

    Add Task 03

---
+++ Seminar_04/Task_03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2.
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр 
+// в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int InputNumberWithMessage(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int A = InputNumberWithMessage("Введите число: ");
+
+int index = (int)Math.Log10(A) + 1;
+
+System.Console.WriteLine(index);
