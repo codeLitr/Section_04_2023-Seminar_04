99b591f2fc41fbe32195cc7349231ec6135f816e
SMV <kraft088@gmail.com>
2023-03-09 21:38:56 +1100

    Add Task 02

---
+++ Seminar_04/Task_02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1:
+// Напишите программу, которая принимает на вход число (А) и выдаёт сумму 
+// чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+int InputNumberWithMessage(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummDigitsInNumber(int number) //Функция сложения чисел от 1 до N.
+{
+    int summ = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        summ += i;
+    }
+    return summ;
+}
+int A = InputNumberWithMessage("Введите число А");
+
+System.Console.WriteLine(SummDigitsInNumber(A));
